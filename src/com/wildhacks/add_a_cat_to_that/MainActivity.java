66be512f8a118e8923f54ec9295dd399b6bb33ee
Kevin Castor <kcas88@gmail.com>
2014-11-22 20:19:27 -0600

    Added simple compositing

--- src/com/wildhacks/add_a_cat_to_that/MainActivity.java
+++ src/com/wildhacks/add_a_cat_to_that/MainActivity.java
@@ -2,11 +2,16 @@ package com.wildhacks.add_a_cat_to_that;
 
 import java.io.File;
 
+import com.wildhacks.add_a_cat_to_that.util.CapturePhotoUtils;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -21,6 +26,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	private static final int TAKE_PICTURE = 0;
 	protected Uri imageUri;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,59 +51,87 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	/** Called when the user clicks the Send button */
 	public void takeImage(View view) {
-	    // Do something in response to button
+		// Do something in response to button
 		System.out.println("Button clicked!");
 		takePicture(view);
-		//Intent intent = new Intent(this, DisplayMessageActivity.class);
+		// Intent intent = new Intent(this, DisplayMessageActivity.class);
 	}
-	
+
 	public void takePicture(View view) {
-	    Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-	    File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
-	    intent.putExtra(MediaStore.EXTRA_OUTPUT,
-	            Uri.fromFile(photo));
-	    imageUri = Uri.fromFile(photo);
-	    startActivityForResult(intent, TAKE_PICTURE);
+		Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+		File photo = new File(Environment.getExternalStorageDirectory(),
+				"Pic.jpg");
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+		imageUri = Uri.fromFile(photo);
+		startActivityForResult(intent, TAKE_PICTURE);
 	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    super.onActivityResult(requestCode, resultCode, data);
-	    switch (requestCode) {
-	    case TAKE_PICTURE:
-	        if (resultCode == Activity.RESULT_OK) {
-	            Uri selectedImage = imageUri;
-	            getContentResolver().notifyChange(selectedImage, null);
-	            ImageView imageView = (ImageView) findViewById(R.id.imageReturn);
-	            ContentResolver cr = getContentResolver();
-	            Bitmap bitmap;
-	            try {
-	                bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, selectedImage);
-	                bitmap = getResizedBitmap(bitmap, 500);
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case TAKE_PICTURE:
+			if (resultCode == Activity.RESULT_OK) {
+				Uri selectedImage = imageUri;
+				getContentResolver().notifyChange(selectedImage, null);
+				ImageView imageView = (ImageView) findViewById(R.id.imageReturn);
+				ContentResolver cr = getContentResolver();
+				Bitmap bitmap;
+				try {
+					System.out.println("build image");
+					bitmap = android.provider.MediaStore.Images.Media
+							.getBitmap(cr, selectedImage);
+					//String imageUri = "drawable://" + R.drawable.cat_above;
+					bitmap = getResizedBitmap(bitmap, 800);
+					Bitmap overlay = BitmapFactory.decodeResource(this.getResources(),
+                            R.drawable.square_cat);
+					bitmap = compositeBitmap(bitmap, overlay);	
 
-	                imageView.setImageBitmap(bitmap);
-	                Toast.makeText(this, selectedImage.toString(),
-	                        Toast.LENGTH_LONG).show();
-	            } catch (Exception e) {
-	                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-	                        .show();
-	                Log.e("Camera", e.toString());
-	            }
-	        }
-	    }
+					/* (ContentResolver cr, 
+					Bitmap source, 
+					String title, 
+					String description) {
+					*/
+					CapturePhotoUtils.insertImage(cr, bitmap, "cat cat", "cat cat cat cat");
+					imageView.setImageBitmap(bitmap);
+					Toast.makeText(this, selectedImage.toString(),
+							Toast.LENGTH_LONG).show();
+				} catch (Exception e) {
+					Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+							.show();
+					Log.e("Camera", e.toString());
+				}
+			}
+		}
 	}
-	
-	public Bitmap getResizedBitmap(Bitmap bm, int newWidth) {
-	    int width = bm.getWidth();
-	    int height = bm.getHeight();
-	    float aspectRatio = (float)width/(float)height;
-	    int newHeight = (int)Math.round((float)newWidth/(float)aspectRatio);
 
-	    // "RECREATE" THE NEW BITMAP
-	    Bitmap resizedBitmap = Bitmap.createScaledBitmap(bm, newWidth, newHeight, false);
-	    return resizedBitmap;
+	public Bitmap getResizedBitmap(Bitmap bm, int newWidth) {
+		int width = bm.getWidth();
+		int height = bm.getHeight();
+		float aspectRatio = (float) width / (float) height;
+		int newHeight = (int) Math
+				.round((float) newWidth / (float) aspectRatio);
+
+		// "RECREATE" THE NEW BITMAP
+		Bitmap resizedBitmap = Bitmap.createScaledBitmap(bm, newWidth,
+				newHeight, false);
+		return resizedBitmap;
+	}
+
+	public Bitmap compositeBitmap(Bitmap bm, Bitmap compBm) {
+		Bitmap tempBitmap = bm;
+		Bitmap overlay = compBm;
+
+		Bitmap finalBitmap = Bitmap.createBitmap(tempBitmap.getWidth(),
+				tempBitmap.getHeight(), tempBitmap.getConfig());
+
+		Canvas canvas = new Canvas(finalBitmap);
+
+		canvas.drawBitmap(tempBitmap, new Matrix(), null);
+		canvas.drawBitmap(overlay, new Matrix(), null);
+		return finalBitmap;
 	}
 }
