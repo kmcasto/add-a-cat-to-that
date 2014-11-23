217f12abe9431900fd4b697184cf18ab75bdf2d7
Douglas Bailey <DBailey01@gmail.com>
2014-11-23 10:07:54 -0600

    removed heavy cats

--- src/com/wildhacks/add_a_cat_to_that/MainActivity.java
+++ src/com/wildhacks/add_a_cat_to_that/MainActivity.java
@@ -310,14 +310,14 @@ public class MainActivity extends Activity {
 		int[] images = {
 			    R.drawable.c1, R.drawable.c2,
 			    R.drawable.c3, R.drawable.c4,
-			    R.drawable.c5, R.drawable.c6,
+			    //R.drawable.c5, R.drawable.c6,
 			    /*R.drawable.c7,*/ R.drawable.c8,
-			    R.drawable.c9, /*R.drawable.c10,*/
+			    //R.drawable.c9, /*R.drawable.c10,*/
 			    R.drawable.c11, R.drawable.c12,
 			    R.drawable.c13, /*R.drawable.c14,*/
 			    R.drawable.c15, R.drawable.c16,
 			    R.drawable.c17, R.drawable.c18,
-			    R.drawable.c19, R.drawable.c20,
+			    /*R.drawable.c19,*/ R.drawable.c20,
 			    R.drawable.c21, R.drawable.c22,
 			    R.drawable.c23, R.drawable.c24,
 			    R.drawable.c25, R.drawable.c26,
